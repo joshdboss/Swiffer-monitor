f6cd4e9ff72aba613632f46987434fe35af33469
joshdboss <newbie856@gmail.com>
2020-08-24 22:41:53 +0200

    changed to using a class
    also added the plots

--- data_analysis/data_analyzer.py
+++ data_analysis/data_analyzer.py
@@ -12,115 +12,120 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import time
 
-def analyze_file(filepath):
-    """
-
-    Parameters
-    ----------
-    filepath : STRING
-        PATH TO THE FILE ANALYZED.
-
-    Returns
-    -------
-    A dictionary of the analyzed data.
-
-    """
-    data = {}
-    data["filename"] = filepath
+class session:
+    _registry = []
+    _durations = []
+    _weekdays = []
+    _hours = []
     
-    # open file and count number of lines to initialize the array
-    inFile = open(filepath, 'r')
-    lineCount = len(inFile.readlines())
-    data["linecount"] = lineCount
-    fileContents = np.empty([lineCount,10])
-    inFile.seek(0)
-    
-    # read the file data into a numpy array
-    i = 0
-    for line in inFile.readlines():
-        lineTimeRaw = line.split(' ')[0].split('_')
-        seconds, milli = lineTimeRaw[-1].split('.')
-        lineTimeRaw[-1] = seconds
-        lineTimeRaw.append(milli)
-        lineTime = list(map(int, lineTimeRaw))
-        lineDateTime = datetime(
-            lineTime[0], lineTime[1], lineTime[2], lineTime[3], lineTime[4],
-            lineTime[5], lineTime[6])
-        fileContents[i,:] = [lineDateTime.timestamp()] + line.split()[1:]
-        i += 1
+    def __init__(self, filepath):
+        self._registry.append(self)
         
-    # raw data
-    data["rawDateTime"] = np.array(fileContents[:,0], dtype=np.datetime64)
-    data["rawIMU_mag_yaw"] = fileContents[:,1]
-    data["rawIMU_mag_pitch"] = fileContents[:,2]
-    data["rawIMU_mag_roll"] = fileContents[:,3]
-    data["rawIMU_accel_yaw"] = fileContents[:,4]
-    data["rawIMU_accel_pitch"] = fileContents[:,5]
-    data["rawIMU_accel_roll"] = fileContents[:,6]
-    data["rawIMU_gyro_yaw"] = fileContents[:,7]
-    data["rawIMU_gyro_pitch"] = fileContents[:,8]
-    data["rawIMU_gyro_roll"] = fileContents[:,9]
-    
-    # get time parameters for the session
-    data["datetime_start"] = datetime.utcfromtimestamp(fileContents[0,0])
-    data["datetime_end"] = datetime.utcfromtimestamp(fileContents[-1,0])
-    data["datetime_middle"] = datetime.utcfromtimestamp(
-        fileContents[math.ceil(data["linecount"]/2),0]) # for middle of session
-    data["weekday"] = datetime.weekday(data["datetime_middle"]) 
-    data["duration"] = fileContents[-1,0] - fileContents[0,0]
-    
-    
-    
-    return(data)
-
+        # open file and count number of lines to initialize the array
+        inFile = open(filepath, 'r')
+        self.lineCount = len(inFile.readlines())
+        fileContents = np.empty([self.lineCount,10])
+        inFile.seek(0)
+        
+        # read the file data into a numpy array
+        i = 0
+        for line in inFile.readlines():
+            lineTimeRaw = line.split()[0].split('_')
+            seconds, milli = lineTimeRaw[-1].split('.')
+            lineTimeRaw[-1] = seconds
+            lineTimeRaw.append(milli)
+            lineTime = list(map(int, lineTimeRaw))
+            lineDateTime = datetime(
+                lineTime[0], lineTime[1], lineTime[2], lineTime[3], lineTime[4],
+                lineTime[5], lineTime[6])
+            fileContents[i,:] = [lineDateTime.timestamp()] + line.split()[1:]
+            i += 1
+        inFile.close()
+        
+            # raw data
+        self.rawDateTime = np.array(fileContents[:,0], dtype=np.datetime64)
+        self.rawIMU_mag_yaw = fileContents[:,1]
+        self.rawIMU_mag_pitch = fileContents[:,2]
+        self.rawIMU_mag_roll = fileContents[:,3]
+        self.rawIMU_accel_yaw = fileContents[:,4]
+        self.rawIMU_accel_pitch = fileContents[:,5]
+        self.rawIMU_accel_roll = fileContents[:,6]
+        self.rawIMU_gyro_yaw = fileContents[:,7]
+        self.rawIMU_gyro_pitch = fileContents[:,8]
+        self.rawIMU_gyro_roll = fileContents[:,9]
+        
+        # get time parameters for the session
+        self.datetime_start = datetime.utcfromtimestamp(fileContents[0,0])
+        self.datetime_end = datetime.utcfromtimestamp(fileContents[-1,0])
+        self.datetime_middle = datetime.utcfromtimestamp(
+            fileContents[math.ceil(self.lineCount/2),0]) # for middle of session
+        self._hours.append(self.datetime_middle.hour)
+        self.weekday = datetime.weekday(self.datetime_middle) 
+        self._weekdays.append(self.weekday)
+        self.duration = fileContents[-1,0] - fileContents[0,0]
+        self._durations.append(self.duration)
+        
 
 if __name__ == "__main__":
     folderName = os.path.join('c:', os.sep,'Users','newbi','OneDrive','Documents','p&g','Logging')
-    fileInfo = []
     t = time.time()
     for fileName in sorted(os.listdir(folderName)): #go through each file and get its data
         if str.startswith(fileName,'IMULog'):
-            fileInfo.append(analyze_file(os.path.join(folderName,fileName)))
-   # print(fileInfo)
+            session(os.path.join(folderName,fileName))
     elapsed = time.time() - t
-    print(elapsed)
+    print('Took %ss to load all files' % elapsed)
     
-    print(fileInfo[0]["datetime_start"])
-    plt.figure(0)
-    plt.title('mag')
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_mag_roll"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_mag_yaw"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_mag_pitch"])
-    plt.legend(["pitch","roll","yaw"])
+    fig1, ax1 = plt.subplots()
+    plt.title('Day of the week histogram')
+    ax1.hist(session._weekdays, bins = [i for i in range(8)], align='left', ec="k")
+    xticks = [i for i in range(7)]
+    xtick_labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+                    'Saturday', 'Sunday']
+    ax1.set_xticks(xticks)
+    ax1.set_xticklabels(xtick_labels)
+    plt.xlabel('Day of the week')
+    plt.ylabel('Number of cleaning sessions')
     
-    plt.figure(1)
-    plt.title('accel')
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_accel_roll"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_accel_yaw"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_accel_pitch"])
-    plt.legend(["pitch","roll","yaw"])
+    fig2, ax2 = plt.subplots()
+    plt.title('Duration histogram')
+    ax2.hist(session._durations, bins = 25, align='left', ec="k")
+    plt.xlabel('Duration (s)')
+    plt.ylabel('Number of cleaning sessions')
     
-    plt.figure(2)
-    plt.title('gyro')
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_gyro_roll"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_gyro_yaw"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_gyro_pitch"])
-    plt.legend(["pitch","roll","yaw"])
+    fig3, ax3 = plt.subplots()
+    plt.title('Time of day histogram')
+    ax3.hist(session._hours, bins = [i for i in range(25)], align='left', ec="k")
+    xticks = [2*i for i in range(12)]
+    ax3.set_xticks(xticks)
+    plt.xlabel('Time of day (UTC)')
+    plt.ylabel('Number of cleaning sessions')
     
-    plt.figure(3)
-    plt.title('yaw')
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_accel_yaw"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_gyro_yaw"])
-    plt.legend(["accel","gyro"])
     
+    chosen_session = session._registry[0]
     plt.figure(4)
-    plt.title('pitch')
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_accel_pitch"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_gyro_pitch"])
-    plt.legend(["accel","gyro"])
+    plt.title('Magnetometer readings')
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_mag_pitch)
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_mag_roll)
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_mag_yaw)
+    plt.legend(["Pitch","Roll","Yaw"])
+    plt.xlabel('Time')
+    plt.ylabel('Angle (degrees)')
     
     plt.figure(5)
-    plt.title('roll')
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_accel_roll"])
-    plt.plot(fileInfo[2]["rawDateTime"],fileInfo[2]["rawIMU_gyro_roll"])
-    plt.legend(["accel","gyro"])
+    plt.title('Accelerometer readings')
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_accel_pitch)
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_accel_roll)
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_accel_yaw)
+    plt.legend(["Pitch","Roll","Yaw"])
+    plt.xlabel('Time')
+    plt.ylabel('Angle (degrees)')
+    
+    plt.figure(6)
+    plt.title('Gyroscope readings')
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_gyro_pitch)
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_gyro_roll)
+    plt.plot(chosen_session.rawDateTime,chosen_session.rawIMU_gyro_yaw)
+    plt.legend(["Pitch","Roll","Yaw"])
+    plt.xlabel('Time')
+    plt.ylabel('Angle (degrees)')
+    
